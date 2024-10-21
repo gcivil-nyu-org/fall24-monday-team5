9f5e6c1fffd5f663f1fa98c87f8d88f4d5e36ad7
xtwang822 <xtwang2024@hotmail.com>
2024-10-21 16:32:18 -0400

    delete modify

--- calmseek/calmseek/urls.py
+++ calmseek/calmseek/urls.py
@@ -17,13 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('appointments/', include('appointments.urls')),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
