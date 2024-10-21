94b255d5014636ed83bfbb67f3e8a4e18dc99d11
xtwang822 <xtwang2024@hotmail.com>
2024-10-21 14:39:54 -0400

    modify urls

--- calmseek/appointments/urls.py
+++ calmseek/appointments/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('update_appointment/<int:appointment_id>/<int:slot_id>/', views.update_appointment, name='update_appointment'),
 ]
 
-+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
