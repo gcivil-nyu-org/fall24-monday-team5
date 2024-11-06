59d65b1cbc6bd8d0f4a0505a28c21f2cac118540
Aryan Prasad <aryan.sprasad27@gmail.com>
2024-11-05 22:29:06 -0500

    fixed black error

--- calmseek/urls.py
+++ calmseek/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
 from django.urls import path, include
+
 # import appointments.views as views
 
 urlpatterns = [
