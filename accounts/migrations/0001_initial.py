35c5164c0d3b4a683cc280b01d43f4adbc22871d
Aryan Prasad <aryan.sprasad27@gmail.com>
2024-11-06 21:02:01 -0500

    reverting migration file

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("auth", "0012_alter_user_first_name_max_length"),
     ]
 
     operations = [
