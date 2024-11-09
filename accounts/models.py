9028317b64963e7f5a0c565f409b915eb0ae1e58
HANWEN LIAO <56802358+HanwenLiao@users.noreply.github.com>
2024-11-08 21:58:32 -0500

    58/59-hanwen

--- accounts/models.py
+++ accounts/models.py
@@ -16,18 +16,31 @@ class Profile(AbstractUser):
         return f"{self.get_username()} - {self.role}"
 
 
-# this model is for the providers
+
 class Provider(models.Model):
+    MENTAL_HEALTH_SPECIALIZATIONS = [
+        ('Clinical Psychology', 'Clinical Psychology'),
+        ('Counseling Psychology', 'Counseling Psychology'),
+        ('Psychiatry', 'Psychiatry'),
+        ('Child and Adolescent Psychiatry', 'Child and Adolescent Psychiatry'),
+        ('Addiction Psychiatry', 'Addiction Psychiatry'),
+        ('Forensic Psychiatry', 'Forensic Psychiatry'),
+        ('Neuropsychology', 'Neuropsychology'),
+        ('Behavioral Therapy', 'Behavioral Therapy'),
+        ('Cognitive Behavioral Therapy', 'Cognitive Behavioral Therapy'),
+        ('Psychoanalysis', 'Psychoanalysis'),
+        # Add more mental health specializations as needed
+    ]
+
     user = models.OneToOneField(
         Profile, on_delete=models.CASCADE, related_name="provider"
     )
     bio = models.TextField()
     phone_number = models.CharField(max_length=20)
     license_number = models.CharField(max_length=20)
-    specialization = models.CharField(max_length=20)
+    specialization = models.CharField(
+        max_length=50, choices=MENTAL_HEALTH_SPECIALIZATIONS)
     is_activated = models.BooleanField(default=False)
-
-
 # this is the model for client users
 class Client(models.Model):
     user = models.OneToOneField(
