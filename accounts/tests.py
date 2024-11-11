0d9fe4287483efedaa32043f930cfe6d3c08b1bf
Aryan Prasad <aryan.sprasad27@gmail.com>
2024-11-11 14:21:08 -0500

    fixed tests coverage issue

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
-from .models import Profile
-from .forms import ProfileEditForm
+from accounts.models import Profile
+from accounts.forms import ProfileEditForm
 
 User = get_user_model()
 
