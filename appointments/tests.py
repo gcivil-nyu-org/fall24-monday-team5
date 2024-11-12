d70e7c8ae9993df11f3d76d9bef7377ff9372ded
Aryan Prasad <aryan.sprasad27@gmail.com>
2024-11-11 20:23:36 -0500

    corrected flake error

--- appointments/tests.py
+++ appointments/tests.py
@@ -1,7 +1,6 @@
 # Create your tests here.
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth import get_user_model
 from django.utils import timezone
 from datetime import timedelta
 from appointments.models import TimeSlot, Appointment
