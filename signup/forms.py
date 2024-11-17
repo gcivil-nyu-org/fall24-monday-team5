aa8379fc1168b44364c656aac607539d482a26f6
Aryan Prasad <aryan.sprasad27@gmail.com>
2024-11-16 22:33:09 -0500

    resolved black error

--- signup/forms.py
+++ signup/forms.py
@@ -63,7 +63,7 @@ class UserSignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=255, required=True)
-    
+
     class Meta:
         model = Profile
         fields = (
