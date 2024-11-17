27451985eafe71f3acc2311b81e4f3f5c0962943
Aryan Prasad <aryan.sprasad27@gmail.com>
2024-11-16 22:24:38 -0500

    fixed black error

--- calmseek/views.py
+++ calmseek/views.py
@@ -6,10 +6,12 @@ from django.contrib.auth.decorators import login_required
 def error(request):
     return render(request, "error.html")
 
+
 @login_required
 def log_out(request):
     logout(request)
     return redirect("login")
+
 
 def log_in(request):
     login(request)
