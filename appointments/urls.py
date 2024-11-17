2893d47fed322ddf2cd00e694373b7b5f4c3c13f
xtwang822 <xtwang2024@hotmail.com>
2024-11-17 12:04:54 -0500

    add some tests in login

--- appointments/urls.py
+++ appointments/urls.py
@@ -5,7 +5,6 @@ app_name = "appointments"
 
 urlpatterns = [
     path("time_slots/", views.time_slots, name="time_slots"),
-    # path("book/<int:slot_id>/", views.book_appointment, name="book_appointment"),
     path("book/", views.book_appointment, name="book_appointment"),
     path("success/", views.appointment_success, name="appointment_success"),
     path("my-appointments/", views.my_appointments, name="my_appointments"),
--- calmseek/tests.py
+++ calmseek/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from accounts.models import Profile
+from django.contrib.auth.models import AnonymousUser
 
 
 class LoginViewTest(TestCase):
@@ -13,6 +14,7 @@ class LoginViewTest(TestCase):
         )
         self.login_url = reverse("login")
         self.time_slots_url = reverse("appointments:time_slots")
+        self.logout_url = reverse("logout")
 
     def test_login_valid_user(self):
         # Test login with valid credentials
@@ -24,7 +26,7 @@ class LoginViewTest(TestCase):
         self.assertEqual(user.status_code, 200)
 
     def test_login_invalid_user(self):
-        """Test login with invalid credentials."""
+        # Test login with invalid credentials.
         response = self.client.post(
             self.login_url, {"username": "normal_user", "password": "wrongpassword"}
         )
@@ -35,3 +37,18 @@ class LoginViewTest(TestCase):
             "Please enter a correct username and password. Note that both fields may be case-sensitive.",  # noqa: E501
         )
         self.assertTemplateUsed(response, "registration/login.html")
+
+    def test_access_protected_page_without_login(self):
+        # Test that trying to access a protected page without logging in redirects.
+        response = self.client.get(self.time_slots_url, follow=True)
+        self.assertRedirects(response, f"{self.login_url}?next={self.time_slots_url}")
+        self.assertTemplateUsed(response, "registration/login.html")
+
+    def test_logout(self):
+        # Test logout functionality.
+        self.client.login(username="normal_user", password="pass")
+        # Log out
+        response = self.client.get(self.logout_url)
+        self.assertRedirects(response, self.login_url)
+        user = response.wsgi_request.user
+        self.assertTrue(isinstance(user, AnonymousUser))
