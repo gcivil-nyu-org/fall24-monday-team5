94b5f6c66d6f9d8e1f26de28e2c01129748d5c4e
ShambhaviSeth <shambhavi22seth@gmail.com>
2024-11-18 20:35:37 -0500

    Fixed photo issue

--- calmseek/urls.py
+++ calmseek/urls.py
@@ -36,5 +36,4 @@ urlpatterns = [
     path("home/", views.home, name="home"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
