eebb0189a723d4bf042d2a934b72bb516e671b1e
xtwang822 <xtwang2024@hotmail.com>
2024-12-05 17:34:08 -0500

    modify

--- groups/admin.py
+++ groups/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Group, GroupMessage
 
+
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("name", "created_by", "created_at")
